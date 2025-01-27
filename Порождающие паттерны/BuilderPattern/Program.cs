fa1c6710d79bdfca3e43282414fa8adea1615ee4
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2025-01-27 23:59:15 +0300

    Созданы заготовочные проекты для основных(остальных) Порождающих паттернов

---
+++ Порождающие паттерны/BuilderPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BuilderPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Строитель (Builder) - шаблон проектирования, который инкапсулирует создание объекта
+            // и позволяет разделить его на различные этапы.
+        }
+    }
+}
