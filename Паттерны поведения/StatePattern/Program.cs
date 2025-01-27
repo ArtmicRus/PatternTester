9036cfa29b28a6019c31db0a5e47bc77e6aa8db7
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2025-01-28 02:42:28 +0300

    Созданы заготовочные проекты для основных(остальных) Паттернов поведения

---
+++ Паттерны поведения/StatePattern/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StatePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Состояние (State) - шаблон проектирования, который позволяет объекту изменять свое поведение
+            // в зависимости от внутреннего состояния.
+        }
+    }
+}
