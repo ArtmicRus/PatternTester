f18c72465a46156706f4f645c6c6e820f806fcf0
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2025-02-01 01:37:51 +0300

    Добавлен пример паттерна Memento

---
+++ Паттерны поведения/MementoPattern/Examples/Metanit/Entity/GameHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace MementoPattern.Examples.Metanit.Entity
+{
+    class GameHistory
+    {
+        public Stack<HeroMemento> History { get; private set; }
+        public GameHistory()
+        {
+            History = new Stack<HeroMemento>();
+        }
+    }
+}
--- Паттерны поведения/MementoPattern/Program.cs
+++ Паттерны поведения/MementoPattern/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MementoPattern
+﻿using MementoPattern.Examples.Metanit.Entity;
+
+namespace MementoPattern
 {
     internal class Program
     {
@@ -6,6 +8,18 @@
         {
             // Паттерн Хранитель (Memento) позволяет выносить внутреннее состояние объекта
             // за его пределы для последующего возможного восстановления объекта без нарушения принципа инкапсуляции.
+
+            Hero hero = new Hero();
+            hero.Shoot(); // делаем выстрел, осталось 9 патронов
+            GameHistory game = new GameHistory();
+
+            game.History.Push(hero.SaveState()); // сохраняем игру
+
+            hero.Shoot(); //делаем выстрел, осталось 8 патронов
+
+            hero.RestoreState(game.History.Pop());
+
+            hero.Shoot(); //делаем выстрел, осталось 8 патронов
         }
     }
 }
